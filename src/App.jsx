3e47bcd6b4b1b484d754a50de64090d440d6a29c
GohiToki510 <gohitoki_micro1004@outlook.com>
2026-01-30 13:40:49 +0900

    koukai1

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import MonsterPage from "./MonsterPage";
 import ScrollTop from "./scrollTop";
